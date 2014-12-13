947eeaa439cf66dc438b000a21cf487bd9a1f0b3
Semen Bevzuk <semen.bevzuk@gmail.com>
2014-12-13 18:57:09 +0300

    Сортировка выбором и вставками

--- lab_1.pas
+++ lab_1.pas
@@ -1,20 +1,120 @@
 program lab_1;
-uses CRT;
-
+//uses CRT;
+const
+  max_size=10000;
 type 
-  array_1: array [1..N]of integer;
-procedure filling_array(a:array_1; n,min,max:integer);
+  array_1=array [1..max_size]of integer;
+  
+procedure filling_array(var a:array_1; n,min,max:integer);
 var i:integer;
 begin
   for i:=1 to n do
   begin
-    a[i]:=random(max)+1-min;
+    a[i]:=random(max-min+1)+min;
   end;
 end;
-procedure ();
 
-var 
+procedure output_array(a:array_1; n:integer);
+var i:integer;
+begin
+  for i:=1 to n do
+  begin
+    write(a[i],' ');
+  end;
+  writeln();
+end;
+procedure input_array(var a:array_1; n:integer);
+var i:integer;
+begin
+  for i:=1 to n do
+  begin
+    write('Ââåäèòå ', i, ' ýëåìåíò: ');
+    readln(a[i]);
+  end;
+end;
+
+//procedure linear_search(a:array_1; n:integer; elm:integer); //ÏÎÈÑÊ
+//procedure binary_search(a:array_1; n:integer; elm:integer);
+
+procedure swap_elm(var a:array_1; pos_1,pos_2:integer);
+var k:integer;
+begin
+  k:=a[pos_1];
+  a[pos_1]:=a[pos_2];
+  a[pos_2]:=k;
+end;
+
+procedure max_array(var a:array_1; n:integer; var max:integer);
+var i:integer;
+begin
+  max:=a[1];
+  for i:=2 to n do
+  begin
+    if a[i]>max then 
+      max:=a[i];
+  end;
+end;
+
+procedure find_min_array(var a:array_1; start, n:integer; var pos_min:integer);
+var i,min:integer;
+begin
+  min:=a[start];
+  pos_min:=start;
+  for i:=start to n do
+  begin
+    if min>a[i] then
+    begin
+      min:=a[i];
+      pos_min:=i;
+    end;
+  end;
+end;
+{/////////////////////////////////////////////ÑÎÐÒÈÐÎÂÊÈ///////////////////////}
+procedure sort_choice(var a:array_1; n:integer);
+var i,j,min,pos_min:integer;
+begin
+  for i:=1 to n-1 do
+    for j:=i to n do
+    begin
+      find_min_array(a,j,n,pos_min);
+      swap_elm(a,pos_min,j);
+    end;
+end;
+
+procedure sort_insert(var a:array_1; n:integer);
+var i,elm,pos,p:integer;
+begin
+  for i:=2 to n do
+  begin
+    elm:=a[i];
+    p:=i-1;
+    while (p>0) do
+    begin
+      if a[p]>elm then 
+      begin
+        a[p+1]:=a[p];
+        p:=p-1;
+      end
+      else
+        break;
+    end;
+    a[p+1]:=elm;
+  end;
+end;
+
+var a:array_1;
+    a_sort:array_1;
+i,j,min_value,max_value,n:integer;
 
 begin
-
+  n:=10;
+  min_value:=-10;
+  max_value:=10;
+  filling_array(a,n,min_value,max_value);
+  a_sort:=a;
+  output_array(a,n);
+  writeln();
+  //sort_choice(a,n);
+  sort_insert(a,n);
+  output_array(a,n);
 end.
